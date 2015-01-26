2d521937a6bf43dd137466983aa25adb35d0fa0c
softprops <d.tangren@gmail.com>
2015-01-25 19:38:16 -0500

    flush this out a bit more

--- src/main/scala/Yo.scala
+++ src/main/scala/Yo.scala
@@ -1,50 +1,117 @@
 package yoyo
 
-import com.ning.http.client.Response
-import dispatch.{ as, Http, Req, :/ }
-import org.json4s.{ JObject, JInt, JString }
+import com.ning.http.client.{ AsyncHandler, Response }
+import dispatch.{ as, FunctionHandler, Http, Req, :/ }
+import org.json4s._
 import scala.concurrent.{ ExecutionContext, Future }
+
+trait Rep[T] {
+  def map(value: Response): T
+}
+
+object Rep {
+  implicit val identity: Rep[Response] =
+    new Rep[Response] {
+      def map(value: Response) = value
+    }
+}
+
+object Yo {
+  type Handler[T] = AsyncHandler[T]
+
+  abstract class Completion[T: Rep] {
+    def apply[TT]
+      (handler: Handler[TT]): Future[TT]
+    def apply(): Future[T] =
+      apply(implicitly[Rep[T]].map(_))
+    def apply[TT]
+      (f: Response => TT): Future[TT] =
+        apply(new FunctionHandler(f))
+  }
+}
 
 /** justyo - http://dev.justyo.co/yo/documents.html */
 case class Yo(
   token: String, http: Http = new Http)
  (implicit ec: ExecutionContext) {
-  private[this] val credentials = Map("api_token" -> token)
+
+  private[this] val credentials =
+    Map("api_token" -> token)
 
   private[this] def base = :/("api.justyo.co")
 
-  private[this] def sign(req: Req): Req =
+  def complete[A: Rep]
+   (req: Req): Yo.Completion[A] =
+    new Yo.Completion[A] {
+      def apply[T](hand: Yo.Handler[T]) =
+        request(req)(hand)
+    }
+
+  def sign(req: Req): Req =
     if ("GET" == req.toRequest.getMethod)
       req <<? credentials
     else req << credentials
 
-  private[this] def request[T]
+  def request[T]
    (req: Req)
-   (transform: Response => T) = http(
-     sign(req / "") OK transform
-   )
+   (hand: Yo.Handler[T]) =
+     http(sign(req / "") > hand)
 
-  def all = request(base.POST / "yoall") (as.json4s.Json andThen {
-    _ => true
-  })
+  object yo {
+    case class User(
+      name: String,
+      _link: Option[String]           = None,
+      _location: Option[(Long, Long)] = None)
+      extends Yo.Completion[Response] {
+      def link(l: String) = copy(_link = Some(l))
+      def location(lat: Long, lon: Long) = copy(_location = Some((lat, lon)))
+      def apply[T](hand: Yo.Handler[T]): Future[T] =
+        request(base.POST / "yo" << Map("username" -> name))(hand)
+    }
 
-  def yo(username: String) = request(
-    base.POST / "yo" << Map("username" -> username)
-  ) (as.json4s.Json andThen {
-    for {
-      JObject(fields)          <- _
-      ("result", JString(res)) <- fields
-    } yield "OK" == res
-  })
+    case class All(link: Option[String] = None)
+      extends Yo.Completion[Response] {
+      def apply[T](hand: Yo.Handler[T]): Future[T] =
+        request(base.POST / "yoall")(hand)
+    }
 
-  def subscribers = request(
-    base / "subscribers_count"
-  ) (as.json4s.Json andThen {
-    for {
-      JObject(fields)         <- _
-      ("result", JInt(count)) <- fields
-    } yield count.toInt
-  })
+    def user(name: String) = User(name)
+
+    def all = All()
+  }
+
+  object subscriber {
+    def count =
+      complete[Response](base / "subscribers_count")
+  }
+
+  object account {
+    case class Create(
+      name: String, passcode: String,
+      callback: Option[String]       = None,
+      email: Option[String]          = None,
+      description: Option[String]    = None,
+      needsLocation: Option[Boolean] = None)
+    extends Yo.Completion[Response] {
+      def apply[T](hand: Yo.Handler[T]): Future[T] =
+        request(base.POST / "accounts"
+                << Map(
+                  "new_account_username" -> name,
+                  "new_account_passcode" -> passcode)
+                  ++ callback.map(("callback_url" -> _))
+                  ++ email.map(("email" -> _))
+                  ++ description.map(("description" -> _))
+                  ++ needsLocation.map(("needs_location" -> _.toString)))(hand)
+    }
+
+    def create(name: String, password: String) =
+      Create(name, password)
+
+    def exists(name: String) =
+      complete[Response](
+        base / "check_username"
+        <<? Map("username" -> name))      
+  }
 
   def close() = http.shutdown()
 }
